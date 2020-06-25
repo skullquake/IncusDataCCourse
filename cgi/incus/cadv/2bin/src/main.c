eaf176d6c8de2e4eb67a971b33c7c7e97abfd4c5
skullquake <ockert8080@gmail.com>
2020-06-25 02:24:25 -0700

    .

--- cgi/incus/cadv/2bin/src/main.c
+++ cgi/incus/cadv/2bin/src/main.c
@@ -1,5 +1,5 @@
 /*!@file: ./src/main.c
- * @brief: description
+ * @brief: prints out binary representation of data
  */
 #include<stdio.h>
 #include<stdlib.h>
@@ -17,16 +17,27 @@ int main(int argc,char**argv,char**envp){
 	unsigned short us=-1;
 	signed int     si=-1;
 	unsigned int   ui=-1;
-	tobin(&uc,sizeof(uc),stdout);
-	tobin(&uc,sizeof(uc),stdout);
-	tobin(&ss,sizeof(ss),stdout);
-	tobin(&us,sizeof(us),stdout);
-	tobin(&si,sizeof(si),stdout);
-	tobin(&ui,sizeof(ui),stdout);
+	signed long    sl=-1;
+	unsigned long  ul=-1;
+	float           f=1.23;
+	double          d=1.23;
+	long double    ld=1.23;
+	printf("uc: ");tobin(&uc,sizeof(uc),stdout);
+	printf("sc: ");tobin(&uc,sizeof(uc),stdout);
+	printf("us: ");tobin(&ss,sizeof(ss),stdout);
+	printf("ss: ");tobin(&us,sizeof(us),stdout);
+	printf("ui: ");tobin(&si,sizeof(si),stdout);
+	printf("si: ");tobin(&ui,sizeof(ui),stdout);
+	printf("sl: ");tobin(&sl,sizeof(sl),stdout);
+	printf("ul: ");tobin(&ul,sizeof(ul),stdout);
+	printf(" f: ");tobin(&f,sizeof(f),stdout);
+	printf(" d: ");tobin(&d,sizeof(d),stdout);
+	printf("ld: ");tobin(&ld,sizeof(ld),stdout);
 	return EXIT_SUCCESS;
 }
 void tobin(void*data,size_t sz,FILE*fp){
 	for(size_t i=0;i<sz;++i){
+		//todo: improve
 		fprintf(fp,"%d",((*(((unsigned char*)data)+i))&0x80)>0);
 		fprintf(fp,"%d",((*(((unsigned char*)data)+i))&0x40)>0);
 		fprintf(fp,"%d",((*(((unsigned char*)data)+i))&0x20)>0);
