a602837ec8fa037e8105df59aa027969e4285928
skullquake <ockert8080@gmail.com>
2020-07-01 18:02:26 +0200

    .

--- cgi/incus/cppbeg/stack_heap/doc/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++
@@ -1,8 +0,0 @@
-var dir_68267d1309a1af8e8297ef4c3efbcdba =
-[
-    [ "main.cpp", "main_8cpp_source.html", null ],
-    [ "myclib.c", "myclib_8c_source.html", null ],
-    [ "myclib.h", "myclib_8h_source.html", null ],
-    [ "mycpplib.cpp", "mycpplib_8cpp_source.html", null ],
-    [ "mycpplib.h", "mycpplib_8h_source.html", null ]
-];
